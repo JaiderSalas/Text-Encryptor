71eb73b8cfeeb9f7b339d7f593df12d33019b134
JaiderSalas <jsalasb1@unicartagena.edu.co>
2023-01-15 16:32:24 -0500

    funciones creadas

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function encriptar(){
    var texto = document.querySelector("textarea").value
-   
+   var a = texto.replace(/e/g,"enter")
+   var b = a.replace(/i/g,"imes")
+   var c = b.replace(/a/g,"ai")
+   var d = c.replace(/o/g,"ober")
+   var e = d.replace(/u/g,"ufat")
+   console.log(e)
+}
+
+function desencriptar(){
+   var texto = document.querySelector("textarea").value
+   var a = texto.replace(/enter/g,"e")
+   var b = a.replace(/imes/g,"i")
+   var c = b.replace(/ai/g,"a")
+   var d = c.replace(/ober/g,"o")
+   var e = d.replace(/ufat/g,"u")
+   console.log(e)
 }
